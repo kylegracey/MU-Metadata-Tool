f8828adc56f506f51e249383f93f3c113b83df22
Kyle Gracey <kyle@kylegracey.com>
2017-09-07 15:58:14 -0500

    Trying to revert again

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const evalTags = require('./modules/eval-tags')
 // Input and Output Options
 // const inputPath = process.argv[2]
 // const outputPath = process.argv[3]
-const inputPath = './files/brisknewmd.json'
-const outputPath = './files/briskoutput.csv'
+const inputPath = './files/mutest.json'
+const outputPath = './files/mutestoutput.csv'
 const jsonData = require("./" + inputPath)
 
 // Brand Settings
